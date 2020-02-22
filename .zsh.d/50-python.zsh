24dfa6abe66f0b962d907a46d64852c892f39218
pushittoprod <pushittoprod@gmail.com>
2020-02-22 08:57:46 -0800

    extend venv command to search parent directories for virtualenvs

--- .zsh.d/50-python.zsh
+++ .zsh.d/50-python.zsh
@@ -1,8 +1,28 @@
 [[ "$DEBUG" == "1" ]] && echo Configuring Python helpers
 
-venv() {
+# recurse up the directory tree to find a virtualenv directory
+find_venv() {
+  # on principle, I refuse to support virtualenvs in /
+  if [[ "$(pwd)" == / ]]; then
+    return
+  fi
+
   if [[ -d venv ]]; then
-    source venv/bin/activate
+    pwd
+    return
+  fi
+
+  pushd .. >/dev/null
+  find_venv
+  popd >/dev/null
+}
+
+venv() {
+  local -r venv_location="$(find_venv)"
+
+  if [[ "$venv_location" ]]; then
+    echo "Activating virtualenv in ${venv_location}"
+    source "${venv_location}/venv/bin/activate"
     return
   fi
 
