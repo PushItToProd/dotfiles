b42e2a4b5a929ecc827b55335f6960cfedaae4f4
pushittoprod <pushittoprod@gmail.com>
2020-05-09 08:23:17 -0700

    add more short git aliases

--- .zsh.d/50-aliases-tools.zsh
+++ .zsh.d/50-aliases-tools.zsh
@@ -9,6 +9,11 @@ alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo
 
 alias gdiff='git diff --no-index --color-words'
 alias g=git
+alias gs='git s'
+alias gd='git d'
+alias gds='git ds'
+alias gc='git c'
+alias gca='git ca'
 
 alias l=less
 
