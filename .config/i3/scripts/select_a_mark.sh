8488e74e7312664d68ca408786057d8868c825e9
pushittoprod <pushittoprod@gmail.com>
2020-05-22 09:15:48 -0700

    allow select_a_mark to run custom commands and use it for unmarking

--- .config/i3/scripts/select_a_mark.sh
+++ .config/i3/scripts/select_a_mark.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# by default we use the mark to determine what to focus
+cmd='[con_mark="%s"] focus'
+if [[ "$1" ]]; then
+  cmd="$1"
+fi
+
+prompt='Select a mark'
+if [[ "$2" ]]; then
+  prompt="$2"
+fi
+
+
 get_marked() {
   bash ~/.config/i3/scripts/get_marked.sh "$1"
 }
@@ -7,7 +19,7 @@ get_marked() {
 display_menu() {
   i3-msg -t get_marks | jq -r '.[]' | sort | while read -r mark; do
     echo "$mark --- $(get_marked "$mark")"
-  done | rofi -dmenu -i -p "Select a mark"
+  done | rofi -dmenu -i -p "$prompt"
 }
 
 selection="$(display_menu)"
@@ -21,4 +33,4 @@ echo "selected: $selection" >&2
 mark="${selection%% --- *}"
 echo "selected mark: $mark" >&2
 
-i3-msg "$(printf '[con_mark="%s"] focus' "$mark")"
+i3-msg "$(printf "$cmd" "$mark")"
