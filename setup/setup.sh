c723478008b49cc284d7866b3205124900b025ae
pushittoprod <pushittoprod@gmail.com>
2020-08-13 16:36:35 -0700

    add some i3 bar stuff

--- setup/setup.sh
+++ setup/setup.sh
@@ -46,6 +46,11 @@ apt_packages=(
   i3xrocks-net-traffic
   i3xrocks-cpu-usage
   i3xrocks-time
+  i3xrocks-memory
+  i3xrocks-battery
+  i3xrocks-volume
+  i3xrocks-temp
+  i3xrocks-media-player
   i3-gaps-wm
   regolith-gnome-flashback
   regolith-i3-gaps-config
