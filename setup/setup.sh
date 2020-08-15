b2149b8b9a0176f9ed4c31409681da629c8e280e
pushittoprod <pushittoprod@gmail.com>
2020-08-15 09:10:51 -0700

    make rofimoji install idempotent

--- setup/setup.sh
+++ setup/setup.sh
@@ -210,17 +210,21 @@ install_deb --url "$discord_url" --file "$discord_filename" --name discord
 
 notice "Installing Rofimoji for i3"
 
-# Package URL, filename, and download location
-rofimoji_url='https://github.com/fdw/rofimoji/releases/download/4.2.0/rofimoji-4.2.0-py3-none-any.whl'
-rofimoji_package="$(basename "$rofimoji_url")"
-rofimoji="$TMP/$rofimoji_package"
+if sudo -u "$USER" pip3 show rofimoji >/dev/null; then
+  info "rofimoji is already installed"
+else
+  # Package URL, filename, and download location
+  rofimoji_url='https://github.com/fdw/rofimoji/releases/download/4.2.0/rofimoji-4.2.0-py3-none-any.whl'
+  rofimoji_package="$(basename "$rofimoji_url")"
+  rofimoji="$TMP/$rofimoji_package"
 
-# Download the package
-wget -O "$rofimoji" https://github.com/fdw/rofimoji/releases/download/4.2.0/rofimoji-4.2.0-py3-none-any.whl
+  # Download the package
+  wget -O "$rofimoji" https://github.com/fdw/rofimoji/releases/download/4.2.0/rofimoji-4.2.0-py3-none-any.whl
 
-# Install rofimoji. --no-warn-script-location is here because ~/.local/bin isn't
-# on the path for the root user.
-sudo -u "$USER" pip3 install --user --no-warn-script-location "$rofimoji"
+  # Install rofimoji. --no-warn-script-location is here because ~/.local/bin isn't
+  # on the path for the root user.
+  sudo -u "$USER" pip3 install --user --no-warn-script-location "$rofimoji"
+fi
 
 ### Install Virtualenv ###
 notice "Installing Virtualenv"
