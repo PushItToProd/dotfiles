9105aceee7d0983eca9704886a5fd8fad8cf13ce
pushittoprod <pushittoprod@gmail.com>
2020-11-28 09:04:04 -0800

    python version in venv util

--- .zsh.d/50-python-venv.zsh
+++ .zsh.d/50-python-venv.zsh
@@ -39,7 +39,7 @@ venv() {
   read "create_venv?venv not available. create one? (Y/n) "
   if [[ "$create_venv" =~ ^[Yy] ]] || [[ "$create_venv" = "" ]]; then
     echo "Creating virtualenv" >&2
-    python3.8 -m venv venv
+    python3.9 -m venv venv
     source venv/bin/activate
   fi
 }
