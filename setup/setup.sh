afe9ba93b95b5a9836c53c745373b7611ec5b9cf
pushittoprod <pushittoprod@gmail.com>
2020-11-28 09:04:13 -0800

    install Go

--- setup/setup.sh
+++ setup/setup.sh
@@ -353,3 +353,15 @@ else
   tar -xf "$trilium_tar"
   mv -f "$trilium_dirname" "$APPDIR"
 fi
+
+### Install Go ###
+notice "Installing Go"
+go_url='https://golang.org/dl/go1.15.5.linux-amd64.tar.gz'
+go_tar="$(basename "$go_url")"
+if [[ -d /usr/local/go ]]; then
+  info 'Go is already installed'
+else
+  info 'Installing Go'
+  wget "$go_url"
+  tar -C /usr/local -xzf "$go_tar"
+fi
