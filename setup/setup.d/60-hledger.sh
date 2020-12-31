83f8fa5e224087aca8b1155ba8866e194fd1a505
pushittoprod <pushittoprod@gmail.com>
2020-12-31 08:28:10 -0800

    add quotes

--- setup/setup.d/60-hledger.sh
+++ setup/setup.d/60-hledger.sh
@@ -5,7 +5,7 @@ if [[ -f "$USER_HOME/.local/bin/hledger" ]]; then
   info "hledger is already installed"
 else
   # hledger-install.sh is included in the repo for ease of distribution
-  as_me bash /home/$USER/bin/hledger-install.sh
+  as_me bash "$USER_HOME/bin/hledger-install.sh"
 fi
 
 info "Installing hledger-shell"
