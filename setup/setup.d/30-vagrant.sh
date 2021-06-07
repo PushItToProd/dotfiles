5a665f9009f8cc90f073bf2041d11b9cab31370d
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2021-06-07 06:45:41 -0700

    install vagrant

---
+++ setup/setup.d/30-vagrant.sh
@@ -0,0 +1,4 @@
+# via https://www.vagrantup.com/downloads
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+apt_packages+=(vagrant)
