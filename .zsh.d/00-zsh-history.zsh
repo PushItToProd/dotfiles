cd7568a41b4a49c85d594819c044c4d2691b53a8
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2021-07-10 08:28:28 -0700

    add a sample zshaddhistory func but comment it out

--- .zsh.d/00-zsh-history.zsh
+++ .zsh.d/00-zsh-history.zsh
@@ -3,3 +3,13 @@ HISTSIZE=90000
 SAVEHIST=150000
 setopt appendhistory      # append history instead of overwriting
 setopt incappendhistory   # immediately append commands to the history file
+
+# example from the zsh docs - write a per-directory history
+# zshaddhistory() {
+#   print -sr -- ${1%%$'\n'}
+#   fc -p .zsh_local_history
+# }
+
+# localhist() {
+#   tail .zsh_local_history
+# }
