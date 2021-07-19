9f8fed84e05abb22557598479164a75a06679ed6
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2021-07-19 06:43:01 -0700

    custom less/ls alias

--- .zsh.d/50-aliases-system.zsh
+++ .zsh.d/50-aliases-system.zsh
@@ -35,12 +35,47 @@ if [ -x /usr/bin/dircolors ]; then
     alias egrep="egrep $__grep_defaults"
 fi
 
+dir_is_not_empty() {
+  # check visible files
+  {
+    : "$1"/*
+    return 0  # found visible files
+  } always {
+    TRY_BLOCK_ERROR=0
+  }
+
+  # check hidden files
+  {
+    : "$1"/.*
+    return 0
+  } always {
+    TRY_BLOCK_ERROR=0
+  }
+
+  return 1
+} 2>/dev/null
+
+dir_is_empty() {
+  ! dir_is_not_empty "$@"
+}
+
+
+# like ls and less smashed into one
 function llss {
-  if [[ -d "$1" ]]; then
-    ls -l "$1"
-  else
-    less "$1"
+  if [[ $# == 1 ]]; then
+    # if there's one argument and it's a file, open it with less
+    if [[ -f "$1" ]]; then
+      less "$1"
+      return
+    fi
+
+
+    if dir_is_empty "$1"; then
+      echo "empty directory: $1" >&2
+    fi
   fi
+
+  ls "$@"
 }
 
 alias untar='tar --extract --verbose --file'
