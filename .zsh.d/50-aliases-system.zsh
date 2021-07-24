bd03bad47ff9d6edada166cb70b035e21e6a9678
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2021-07-24 07:34:07 -0700

    pull ls/less hackery into its own file

--- .zsh.d/50-aliases-system.zsh
+++ .zsh.d/50-aliases-system.zsh
@@ -11,8 +11,8 @@ alias pbcopy='xclip -sel clip'
 alias pbpaste='xclip -sel clip -o'
 alias open='xdg-open'
 
-alias l=less
-#alias l='ls -F'
+#alias l=less
+alias l='ls -F'
 alias ll='ls -lh'
 alias la='ls -lah'
 alias lt='ls --human-readable --size --sort=size --classify --format=single-column'
@@ -34,49 +34,6 @@ if [ -x /usr/bin/dircolors ]; then
     alias fgrep="fgrep $__grep_defaults"
     alias egrep="egrep $__grep_defaults"
 fi
-
-dir_is_not_empty() {
-  # check visible files
-  {
-    : "$1"/*
-    return 0  # found visible files
-  } always {
-    TRY_BLOCK_ERROR=0
-  }
-
-  # check hidden files
-  {
-    : "$1"/.*
-    return 0
-  } always {
-    TRY_BLOCK_ERROR=0
-  }
-
-  return 1
-} 2>/dev/null
-
-dir_is_empty() {
-  ! dir_is_not_empty "$@"
-}
-
-
-# like ls and less smashed into one
-function llss {
-  if [[ $# == 1 ]]; then
-    # if there's one argument and it's a file, open it with less
-    if [[ -f "$1" ]]; then
-      less "$1"
-      return
-    fi
-
-
-    if dir_is_empty "$1"; then
-      echo "empty directory: $1" >&2
-    fi
-  fi
-
-  ls "$@"
-}
 
 alias untar='tar --extract --verbose --file'
 alias python='python3.9'
