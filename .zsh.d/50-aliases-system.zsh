42f319b757b56e0cc83683dd7471ada9345108ab
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2021-07-24 07:34:58 -0700

    delete commented lines

--- .zsh.d/50-aliases-system.zsh
+++ .zsh.d/50-aliases-system.zsh
@@ -25,8 +25,6 @@ alias egrep='egrep -I --exclude-dir=.git'
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
     alias ls='ls --color=auto'
-    #alias dir='dir --color=auto'
-    #alias vdir='vdir --color=auto'
 
     __grep_defaults='--color=auto -I --exclude-dir=.git --exclude-dir=venv'
 
