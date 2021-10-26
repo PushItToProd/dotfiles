a886d9ac2708ce30d86c71a9382724573f6e552e
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2021-10-26 06:56:02 -0700

    install newer Go version

--- setup/setup.d/60-golang.sh
+++ setup/setup.d/60-golang.sh
@@ -1,11 +1,25 @@
 ### Install Go ###
 notice "Installing Go"
-go_url='https://dl.google.com/go/go1.17.linux-amd64.tar.gz'
+
+GO_VERSION=1.17.2
+
+go_url="https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz"
 go_tar="$(basename "$go_url")"
-if [[ -d /usr/local/go ]]; then
-  info 'Go is already installed'
-else
-  info 'Installing Go'
+
+install_go() {
+  local installed_version=''
+  installed_version="$(</usr/local/go/VERSION)" ||
+
+  if [[ -d /usr/local/go ]]; then
+    if [[ "$installed_version" == "go${GO_VERSION}" ]]; then
+      info 'Go is already installed and up to date'
+      return
+    else
+      info "Go is installed but not the target version -- installed version: $GO_VERSION"
+    fi
+  fi
+  info "Installing Go $GO_VERSION"
   wget "$go_url"
   tar -C /usr/local -xzf "$go_tar"
-fi
+}
+install_go
