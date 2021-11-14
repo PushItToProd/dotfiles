6339d8fd7adf0799e6e3b9652bd418addca01825
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2021-11-14 10:34:44 -0800

    only install hashicorp key if needed

--- setup/setup.d/30-vagrant.sh
+++ setup/setup.d/30-vagrant.sh
@@ -1,5 +1,7 @@
 notice "Setting up Vagrant"
 # via https://www.vagrantup.com/downloads
-curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+if ! apt-key list 2>/dev/null | grep -qi hashicorp; then
+  curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+fi
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 apt_packages+=(vagrant)
