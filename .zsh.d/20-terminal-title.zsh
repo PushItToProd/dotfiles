745c1a347d360ac3c273a5d96ee0ca5687ec144b
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2022-04-03 08:31:02 -0700

    simplify logic and display relative path

--- .zsh.d/20-terminal-title.zsh
+++ .zsh.d/20-terminal-title.zsh
@@ -1,6 +1,3 @@
-__term_title=""
-__term_title_stack=()
-
 set_term_title() {
   __term_title="$*"
   case $TERM in
@@ -11,28 +8,17 @@ set_term_title() {
   esac
 }
 
-push_term_title() {
-  __term_title_stack+=("$__term_title")
-  set_term_title "$*"
-}
-
-pop_term_title() {
-  (( $#__term_title_stack > 0 )) || return
-  set_term_title $__term_title_stack[-1]
-  __term_title_stack[-1]=()
-}
-
 # via 3.6 here: https://zsh.sourceforge.io/FAQ/zshfaq03.html
 set_term_title_to_pwd() {
   [[ -t 1 ]] || return
-  set_term_title "$PWD"
+  set_term_title "$(print -P %~)"
 }
 set_term_title_to_pwd  # run immediately when we're initializing
 chpwd_functions+=(set_term_title_to_pwd)
 
 set_term_title_to_cmd() {
-  push_term_title "$1"
+  set_term_title "$1"
 }
 preexec_functions+=(set_term_title_to_cmd)
 
-precmd_functions+=(pop_term_title)
+precmd_functions+=(set_term_title_to_pwd)
