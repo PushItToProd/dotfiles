4a022433a2b5699e8d53e6f980b3461287296259
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2024-09-12 06:53:56 -0700

    finish converting rofi_code to use mainly Go

--- .config/i3/scripts/rofi_code.zsh
+++ .config/i3/scripts/rofi_code.zsh
@@ -1,8 +1,7 @@
 #!/usr/bin/env zsh
 # rofi_code.zsh implements a custom rofi mode for launching VS Code workspaces.
-
-# FIXME this script feels overly-complicated and could probably stand to be
-# simplified
+# It's mainly a wrapper around my list_vscode_workspaces command implemented in
+# Go.
 
 # TODO implement some way of deleting workspaces that aren't useful (maybe a
 # keybinding)
@@ -10,101 +9,17 @@
 # TODO implement a way of pinning workspaces to the top of the list for easy
 # access.
 
-# XXX alternatively, figure out how to sort workspaces by most recently used.
-# -> It looks like each directory contains a state.vscdb file that gets modified
-# each time the workspace is used.
-
 readonly PROGPATH="${(%):-%N}"
 readonly PROGDIR="${PROGPATH:A:h}"
 readonly WORKSPACE_STORAGE_PATH="$HOME/.config/Code/User/workspaceStorage"
 
-# _list_workspace_files lists all workspace directories sorted by last modified
-# date
-_list_workspace_files() {
-  local state_file
-  local workspace_file
-  find "$WORKSPACE_STORAGE_PATH" -name state.vscdb -printf "%T@ %p\0" |
-    sort --zero-terminated --numeric-sort --reverse |
-    while read -r -d '' _ state_file; do
-      workspace_file="$(dirname "$state_file")/workspace.json"
-      if [[ -f "$workspace_file" ]]; then
-        printf '%s\n' "$workspace_file"
-      fi
-    done
-}
-
-# _list_workspaces calls jq on all the files emitted by _list_workspace_files
-_list_workspaces() {
-  # shellcheck disable=SC2046
-  jq -L"$PROGDIR" -r '
-    include "urldecode";
-    (.folder // .workspace // .configuration.external) | urldecode
-  ' $(_list_workspace_files)
-}
-
-# # dump all workspaces using jq
-# _list_workspaces() {
-#   jq -L"$PROGDIR" -r '
-#     include "urldecode";
-#     (.folder // .workspace // .configuration.external) | urldecode
-#   ' $HOME/.config/Code/User/workspaceStorage/*/workspace.json
-# }
-
-# abbreviate_homedir takes a path and, if it is located within the user's
-# home directory, shortens it to begin with ~
-abbreviate_homedir() {
-  # here the # before $HOME indicates that the pattern must match at the start
-  # of the string (see: man zshexpn)
-  printf '%s' "${1/#$HOME/~}"
-}
-
-# get_friendly_ws_name takes the full path to a workspace and returns a friendly
-# path with the file:// prefix removed and the user's homedir replaced with ~
-get_friendly_ws_name() {
-  local ws="$1"
-  ws="$(strip_file_protocol "$ws")"
-  if [[ "$ws" == "null" ]] || [[ "$ws" == "" ]]; then
-    return
-  fi
-
-  printf '%s\n' "$(abbreviate_homedir "$ws")"
-}
-
-# strip_file_protocol removes the file:// protocol from the start of the given
-# path
-strip_file_protocol() {
-  local ws="$1"
-  if [[ "$ws" == "null" ]]; then
-    return
-  fi
-  ws="${ws#'file://'}"
-  printf '%s\n' "$ws"
-}
-
-# list_workspaces produces a plain list of workspaces for testing purposes
 list_workspaces() {
-  local ws
-  while read -r ws; do
-    echo "$(get_friendly_ws_name "$ws") ($ws)"
-  done < <(_list_workspaces)
-}
-
-# list_workspaces_rofi produces a list of workspaces for rofi to display
-list_workspaces_rofi() {
-  local ws ws_path friendly
-  while read -r ws; do
-    friendly=$(get_friendly_ws_name "$ws")
-    # pass raw workspace name as row info option
-    printf '%s\0info\x1f%s\n' "$friendly" "$ws"
-  done < <(_list_workspaces)
+  go run $HOME/bin/list_vscode_workspaces/cmd/list_workspaces/main.go "$@"
 }
 
 # handle_selection takes the selection from rofi and invokes VS Code with it
 handle_selection() {
-  local selection="$ROFI_INFO"
-
-  local ws_path
-  ws_path="$(strip_file_protocol "$selection")"
+  local ws_path="$ROFI_INFO"
   echo ws_path = $ws_path >&2
 
   # unset rofi's environment variables to prevent issues when testing
@@ -120,9 +35,12 @@ handle_selection() {
 # emits a list of workspaces. If an argument is provided, it launches the
 # corresponding workspace in VS Code.
 rofi_main() {
-  # TODO: use ROFI_RETV to determine behavior
+  # TODO: use ROFI_RETV to determine behavior.
+  # https://davatorium.github.io/rofi/1.7.5/rofi-script.5/#rofi_retv
+
+  # If there are no arugments, list workspaces in a format suitable for rofi.
   if (( $# == 0 )); then
-    list_workspaces_rofi
+    list_workspaces
     return
   fi
 
@@ -140,12 +58,13 @@ main() {
   # if ROFI_RETV is not set, try handling commands from the user
   case "$1" in
     ls|list)
-      list_workspaces
+      list_workspaces -plain
       return
       ;;
   esac
 
-  # otherwise, launch rofi
+  # otherwise, if this was invoked from the CLI with no options, launch rofi and
+  # tell it to use this script.
   rofi -show code -modi "code:$PROGNAME"
 }
 if [[ $ZSH_EVAL_CONTEXT == toplevel ]]; then
