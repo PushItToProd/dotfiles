64648ad6472f7b2adac62abd0bb02eb446b104c8
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2024-09-16 07:02:27 -0700

    make S not a pointer

--- bin/list_vscode_workspaces/pkg/uniquelist/uniqueinserter.go
+++ bin/list_vscode_workspaces/pkg/uniquelist/uniqueinserter.go
@@ -5,33 +5,34 @@ import "slices"
 type Comparator[T any] func(a, b T) int
 
 // SortedInserter uses binary search to insert elements into a slice in sorted
-// order. The slice pointer S must not be nil. If a non-empty slice is provided,
-// it must be in sorted order.
+// order.
 type SortedInserter[T any] struct {
 	// Cmp should be a comparison function that satisfies the definition in
 	// https://pkg.go.dev/slices#BinarySearchFunc.
 	Cmp Comparator[T]
-	S   *[]T
+	// S is the underlying slice. If a non-empty slice is provided, it must be
+	// sorted.
+	S []T
 }
 
 // NewSortedInserter creates a SortedInserter with a new slice of type []T.
 func NewSortedInserter[T any](cmp Comparator[T]) SortedInserter[T] {
 	return SortedInserter[T]{
 		Cmp: cmp,
-		S:   new([]T),
+		S:   []T{},
 	}
 }
 
 // Insert adds an element to the slice in sorted order.
-func (ui SortedInserter[T]) Insert(e T) bool {
-	n, found := slices.BinarySearchFunc(*ui.S, e, func(e, t T) int {
+func (ui *SortedInserter[T]) Insert(e T) bool {
+	n, found := slices.BinarySearchFunc(ui.S, e, func(e, t T) int {
 		return ui.Cmp(e, t)
 	})
 	if found {
 		return false
 	}
 
-	*ui.S = slices.Insert(*ui.S, n, e)
+	ui.S = slices.Insert(ui.S, n, e)
 	return true
 }
 
--- bin/list_vscode_workspaces/pkg/uniquelist/uniqueinserter_test.go
+++ bin/list_vscode_workspaces/pkg/uniquelist/uniqueinserter_test.go
@@ -10,21 +10,14 @@ var intComparator = func(a, b int) int {
 	return a - b
 }
 
-func intInserter(s *[]int) uniquelist.SortedInserter[int] {
-	return uniquelist.SortedInserter[int]{
-		Cmp: intComparator,
-		S:   s,
-	}
-}
-
 func TestInsertRejectsDupes(t *testing.T) {
-	s := []int{}
-	ui := intInserter(&s)
+	ui := uniquelist.NewSortedInserter(intComparator)
 
 	ok := ui.Insert(1)
 	if !ok {
 		t.Errorf("expected insert to be ok, but it was not: %+v", ui)
 	}
+	s := ui.S
 	if len(s) != 1 || s[0] != 1 {
 		t.Errorf("expected slice to look like [1] after insert but got this instead: %+v", s)
 	}
@@ -33,22 +26,24 @@ func TestInsertRejectsDupes(t *testing.T) {
 	if ok {
 		t.Errorf("expected duplicate insert to be rejected")
 	}
+	s = ui.S
 	if len(s) != 1 || s[0] != 1 {
 		t.Errorf("expected slice to look like [1] after insert but got this instead: %+v", s)
 	}
 }
 
 func TestInsertIsSorted(t *testing.T) {
-	s := []int{}
-	ui := intInserter(&s)
+	ui := uniquelist.NewSortedInserter(intComparator)
 
 	ui.Insert(3)
 	ui.Insert(1)
+	s := ui.S
 	if len(s) != 2 || s[0] != 1 || s[1] != 3 {
 		t.Errorf("expected slice to look like [1 3] but got this instead: %v", s)
 	}
 
 	ui.Insert(2)
+	s = ui.S
 	if len(s) != 3 || s[0] != 1 || s[1] != 2 || s[2] != 3 {
 		t.Errorf("expected slice to look like [1 2 3] but got this instead: %v", s)
 	}
@@ -60,7 +55,7 @@ func TestNewSortedInserter(t *testing.T) {
 	ui.Insert(1)
 	ui.Insert(2)
 
-	s := *ui.S
+	s := ui.S
 	if len(s) != 3 || s[0] != 1 || s[1] != 2 || s[2] != 3 {
 		t.Errorf("expected slice to look like [1 2 3] but got this instead: %v", s)
 	}
