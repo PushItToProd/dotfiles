080f6c41928d0fdec7d33cd6fd64cadcdb2abfba
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2024-09-16 06:40:47 -0700

    expand comments

--- bin/list_vscode_workspaces/pkg/uniquelist/uniqueinserter.go
+++ bin/list_vscode_workspaces/pkg/uniquelist/uniqueinserter.go
@@ -4,14 +4,17 @@ import "slices"
 
 type Comparator[T any] func(a, b T) int
 
-// SortedInserter uses binary search to insert elements into a slice in sorted order.
+// SortedInserter uses binary search to insert elements into a slice in sorted
+// order. The slice pointer S must not be nil. If a non-empty slice is provided,
+// it must be in sorted order.
 type SortedInserter[T any] struct {
-	// Cmp should be a comparison function that satifies the definition in
+	// Cmp should be a comparison function that satisfies the definition in
 	// https://pkg.go.dev/slices#BinarySearchFunc.
 	Cmp Comparator[T]
 	S   *[]T
 }
 
+// NewSortedInserter creates a SortedInserter with a new slice of type []T.
 func NewSortedInserter[T any](cmp Comparator[T]) SortedInserter[T] {
 	return SortedInserter[T]{
 		Cmp: cmp,
@@ -19,6 +22,7 @@ func NewSortedInserter[T any](cmp Comparator[T]) SortedInserter[T] {
 	}
 }
 
+// Insert adds an element to the slice in sorted order.
 func (ui SortedInserter[T]) Insert(e T) bool {
 	n, found := slices.BinarySearchFunc(*ui.S, e, func(e, t T) int {
 		return ui.Cmp(e, t)
