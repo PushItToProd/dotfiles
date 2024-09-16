9cfde7b296cdd89d052c204d0a545e732ae6594d
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2024-09-16 07:32:40 -0700

    use slices.Compare instead of manually checking slice entries

--- bin/list_vscode_workspaces/pkg/uniquelist/uniqueinserter_test.go
+++ bin/list_vscode_workspaces/pkg/uniquelist/uniqueinserter_test.go
@@ -1,6 +1,7 @@
 package uniquelist_test
 
 import (
+	"slices"
 	"testing"
 
 	"github.com/pushittoprod/list_vscode_workspaces/pkg/uniquelist"
@@ -19,18 +20,16 @@ func TestInsertRejectsDupes(t *testing.T) {
 	if !ok {
 		t.Errorf("expected insert to be ok, but it was not: %+v", ui)
 	}
-	s := ui.S
-	if len(s) != 1 || s[0] != 1 {
-		t.Errorf("expected slice to look like [1] after insert but got this instead: %+v", s)
+	if slices.Compare(ui.S, []int{1}) != 0 {
+		t.Errorf("expected slice to look like [1] after insert but got this instead: %+v", ui.S)
 	}
 
 	ok = ui.Insert(1)
 	if ok {
 		t.Errorf("expected duplicate insert to be rejected")
 	}
-	s = ui.S
-	if len(s) != 1 || s[0] != 1 {
-		t.Errorf("expected slice to look like [1] after insert but got this instead: %+v", s)
+	if slices.Compare(ui.S, []int{1}) != 0 {
+		t.Errorf("expected slice to look like [1] after insert but got this instead: %+v", ui.S)
 	}
 }
 
@@ -39,15 +38,13 @@ func TestInsertIsSorted(t *testing.T) {
 
 	ui.Insert(3)
 	ui.Insert(1)
-	s := ui.S
-	if len(s) != 2 || s[0] != 1 || s[1] != 3 {
-		t.Errorf("expected slice to look like [1 3] but got this instead: %v", s)
+	if slices.Compare(ui.S, []int{1, 3}) != 0 {
+		t.Errorf("expected slice to look like [1 3] but got this instead: %v", ui.S)
 	}
 
 	ui.Insert(2)
-	s = ui.S
-	if len(s) != 3 || s[0] != 1 || s[1] != 2 || s[2] != 3 {
-		t.Errorf("expected slice to look like [1 2 3] but got this instead: %v", s)
+	if slices.Compare(ui.S, []int{1, 2, 3}) != 0 {
+		t.Errorf("expected slice to look like [1 2 3] but got this instead: %v", ui.S)
 	}
 }
 
@@ -57,8 +54,7 @@ func TestNewSortedInserter(t *testing.T) {
 	ui.Insert(1)
 	ui.Insert(2)
 
-	s := ui.S
-	if len(s) != 3 || s[0] != 1 || s[1] != 2 || s[2] != 3 {
-		t.Errorf("expected slice to look like [1 2 3] but got this instead: %v", s)
+	if slices.Compare(ui.S, []int{1, 2, 3}) != 0 {
+		t.Errorf("expected slice to look like [1 2 3] but got this instead: %v", ui.S)
 	}
 }
