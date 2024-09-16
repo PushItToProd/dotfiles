d0281bae72b456a7a0e7c102b6af595bc646a199
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2024-09-16 06:20:41 -0700

    update a couple comments

--- bin/list_vscode_workspaces/pkg/uniquelist/uniquelist.go
+++ bin/list_vscode_workspaces/pkg/uniquelist/uniquelist.go
@@ -8,10 +8,14 @@ import (
 var DoNotInsert = math.MinInt
 
 type Comparable interface {
-	// Compare shuld be a comparison function that determines whether 
+	// Compare should be a comparison function satisfying the definition in
+	// https://pkg.go.dev/slices#BinarySearchFunc, except it must also return a
+	// bool indicating whether the compared value is compatible and should be
+	// inserted.
 	Compare(other Comparable) (cmp int, ok bool)
 }
 
+// UniqueList is a slice of sorted, unique elements.
 type UniqueList[T Comparable] []T
 
 // Insert inserts an element into the UniqueList if it's not already present
