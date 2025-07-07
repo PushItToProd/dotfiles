62e7b72b43616183500ff164ee2e11af848ee734
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2025-07-07 07:05:13 -0700

    remove old workaround before I figured out AddKeysToAgent

--- .zsh.d/50-ssh.zsh
+++ .zsh.d/50-ssh.zsh
@@ -1,20 +1,7 @@
 # ssh agent helper
-# The file ~/.ssh/ssh_agent.zsh needs to exist and define a function
-# add_ssh_keys which ssh-adds all the desired keys.
-
-if [[ -f ~/.ssh/ssh_agent.zsh ]]; then
-  . ~/.ssh/ssh_agent.zsh
-  if ! typeset -f add_ssh_keys >/dev/null; then
-    echo "warning: 50-ssh.zsh: ~/.ssh/ssh_agent.zsh does not " \
-         "declare the function add_ssh_keys"
-  fi
-else
-  echo "warning: 50-ssh.zsh: ~/.ssh/ssh_agent.zsh does not exist"
-fi
 
 ssh_agent_start() {
   eval "$(ssh-agent | tee ~/.ssh/agent.env)"
-  add_ssh_keys
 }
 
 ssh_agent_restart() {
