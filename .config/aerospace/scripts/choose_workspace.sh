6aa2c6d46d57a540644e8de4c44fa95889fe3075
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2025-12-15 10:01:51 -0800

    include window titles and truncate long titles

--- .config/aerospace/scripts/choose_workspace.sh
+++ .config/aerospace/scripts/choose_workspace.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+MAX_TITLE_LEN=20
+
 FIELDS=(
   'workspace'
   'app-name'
@@ -15,8 +17,24 @@ is_assoc_array() {
   [[ "${__is_assoc_array__var@a}" == *A* ]]
 }
 
+trim_string() {
+    # Usage: trim_string "   example   string    " (emits "example string")
+    : "${1#"${1%%[![:space:]]*}"}"
+    : "${_%"${_##*[![:space:]]}"}"
+    printf '%s\n' "$_"
+}
+
+truncate_str() {
+  local str="$1"
+  local maxlen="${2-$MAX_TITLE_LEN}"
+  printf '%s' "$(trim_string "${str:0:$maxlen}")"
+  if [[ "${#str}" -gt "$maxlen" ]]; then
+    printf '%s' "..."
+  fi
+}
+
 aerospace::list_workspace_windows() {
-  aerospace list-windows --all --format $'%{workspace}\t%{app-name}' | sort
+  aerospace list-windows --all --format $'%{workspace}\t%{app-name}\t%{window-title}' | sort | uniq
 }
 
 group_by_workspace() {
@@ -25,22 +43,25 @@ group_by_workspace() {
   fi
   local -n workspaces="$1"
 
-  local workspace app_name windows
-  while IFS=$'\t' read -r workspace app_name; do
-    windows="${workspaces["$workspace"]}"
-    if [[ ! "$windows" ]]; then
+  local workspace app_name win_title ws_windows app_label
+  while IFS=$'\t' read -r workspace app_name win_title; do
+    win_title="$(truncate_str "$win_title")"
+
+    ws_windows="${workspaces["$workspace"]}"
+    if [[ "$win_title" ]]; then
+      app_label="$app_name ($win_title)"
+    else
+      app_label="$app_name"
+    fi
+
+    if [[ ! "$ws_windows" ]]; then
       # first time seeing this workspace -- add the app name to the start of the
       # list
-      workspaces["$workspace"]="$app_name"
+      workspaces["$workspace"]="$app_label"
       continue
     fi
 
-    # Add app_name to the list if it's not already present. (Edge case: if
-    # there's already an app whose name is a substring of app_name, it won't get
-    # added to the list.)
-    if [[ "$windows" != *"$app_name"* ]]; then
-      workspaces["$workspace"]="$windows, $app_name"
-    fi
+    workspaces["$workspace"]="$ws_windows, $app_label"
   done  # reads from stdin
 }
 
