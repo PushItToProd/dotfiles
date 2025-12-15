198d859bba71fe6764776a204e706738f9e2cc11
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2025-12-15 10:09:06 -0800

    clean up titles

--- .config/aerospace/scripts/choose_workspace.sh
+++ .config/aerospace/scripts/choose_workspace.sh
@@ -2,9 +2,20 @@
 
 MAX_TITLE_LEN=20
 
-FIELDS=(
-  'workspace'
-  'app-name'
+# IGNORE_TITLES specifies window titles that should not be returned
+declare -A IGNORE_TITLES
+IGNORE_TITLES=(
+  ["-zsh"]=1
+  ["bash"]=1
+  ["Mozilla Firefox"]=1
+)
+
+# HIDE_APP_TITLES specifies the names of applications for which titles should
+# not be displayed
+declare -A HIDE_APP_TITLES
+HIDE_APP_TITLES=(
+  ["Activity Monitor"]=1
+  ["KeePassXC"]=1
 )
 
 fatal() {
@@ -37,6 +48,25 @@ aerospace::list_workspace_windows() {
   aerospace list-windows --all --format $'%{workspace}\t%{app-name}\t%{window-title}' | sort | uniq
 }
 
+_generate_app_label() {
+  local app_name="$1" win_title="$2"
+  printf %s "$app_name"
+
+  # skip apps configured not to display titles
+  if [[ "${HIDE_APP_TITLES["$app_name"]}" ]]; then
+    return
+  fi
+
+  win_title="$(truncate_str "$win_title")"
+  if [[ "${IGNORE_TITLES["$win_title"]}" ]]; then
+    return
+  fi
+
+  if [[ "$win_title" ]]; then
+    printf %s " ($win_title)"
+  fi
+}
+
 group_by_workspace() {
   if ! is_assoc_array "$1"; then
     fatal "$0 requires the name of an associative array variable as its first argument"
@@ -45,14 +75,8 @@ group_by_workspace() {
 
   local workspace app_name win_title ws_windows app_label
   while IFS=$'\t' read -r workspace app_name win_title; do
-    win_title="$(truncate_str "$win_title")"
-
     ws_windows="${workspaces["$workspace"]}"
-    if [[ "$win_title" ]]; then
-      app_label="$app_name ($win_title)"
-    else
-      app_label="$app_name"
-    fi
+    app_label="$(_generate_app_label "$app_name" "$win_title")"
 
     if [[ ! "$ws_windows" ]]; then
       # first time seeing this workspace -- add the app name to the start of the
