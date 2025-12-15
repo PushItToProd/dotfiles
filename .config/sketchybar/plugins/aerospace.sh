5e1ea13dcfc8232adb5c961aea8a7945143fefab
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2025-12-15 10:56:47 -0800

    add shebang and tweak comments

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-# make sure it's executable with:
-# chmod +x ~/.config/sketchybar/plugins/aerospace.sh
+# TODO: work out how to make this highlight the correct workspace when I run
+# `sketchybar --reload`. Currently, it leads to no space being highlighted since
+# AEROSPACE_FOCUSED_WORKSPACE (and, thus, FOCUSED_WORKSPACE) is unset.
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
     sketchybar --set "$NAME" background.drawing=on
