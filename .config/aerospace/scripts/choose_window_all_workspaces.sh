087b47e7a1bc191c22d6272ab7995ad7f27089bd
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2025-12-16 07:26:11 -0800

    support picking a window from all workspaces

---
+++ .config/aerospace/scripts/choose_window_all_workspaces.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+# choose_window.sh uses choose-gui to display a list of windows open in the
+# current AeroSpace workspace. The selected window is then focused.
+
+trim_string() {
+    # Usage: trim_string "   example   string    " (emits "example string")
+    : "${1#"${1%%[![:space:]]*}"}"
+    : "${_%"${_##*[![:space:]]}"}"
+    printf '%s\n' "$_"
+}
+
+list_windows() {
+  # The --format argument here is the default value used by aerospace, but we
+  # specify it explicitly in case they ever change it.
+  aerospace list-windows \
+    "$@" \
+    --format '%{window-id}%{right-padding} | %{workspace}%{right-padding} | %{app-name}%{right-padding} | %{window-title}' \
+    | sort --field-separator='|' --ignore-leading-blanks --sort=numeric --key=2
+}
+
+
+main() {
+  aerospace_windows_str=$(list_windows --all)
+
+  # copy window list to an array so we can look up windows by selection index
+  mapfile aerospace_windows <<<"$aerospace_windows_str"
+
+  # remove the first field (window ID) from each displayed option
+  # shellcheck disable=SC2001
+  display_text=$(sed 's/[^|]* \| //' <<<"$aerospace_windows_str")
+
+  # get the selection index
+  selection="$(choose -iz <<< "$display_text")"
+  # echo "selected: ${aerospace_windows["$selection"]}"
+
+  # a selection of -1 means the user hit escape or otherwise exited without
+  # picking an option -> do nothing
+  if [[ "$selection" == -1 ]]; then
+    return
+  fi
+
+  # parse the window_id from the selection and trim surrounding spaces
+  IFS='|' read -r window_id _ <<<"${aerospace_windows["$selection"]}"
+  window_id="$(trim_string "$window_id")"
+
+  # finally, we can switch to the given window
+  aerospace focus --window-id "$window_id"
+}
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+  main "$@"
+fi
