b0e2b1bb96c4eb7837c23cc4b9fbb06005f38aca
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2026-03-19 06:10:52 -0700

    remove ansible-playbook alias

--- .zsh.d/50-aliases-tools.zsh
+++ .zsh.d/50-aliases-tools.zsh
@@ -2,8 +2,6 @@ i3-binds() {
   grep --color=never -e '^[^#]*set' -e '^[^#]*bind' -e '^[^#]*mode' .config/regolith/i3/config
 }
 
-alias ap='ansible-playbook'
-
 randpass() {
   openssl rand -base64 "${1:-32}" | tr -d '\n'
 }
