51dcc825336ba314cf0578f27fe3bd3328dddd22
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2026-03-19 06:09:38 -0700

    add some git aliases

--- .zsh.d/50-aliases-tools.zsh
+++ .zsh.d/50-aliases-tools.zsh
@@ -5,8 +5,11 @@ i3-binds() {
 alias gdiff='git diff --no-index --color-words'
 alias g=git
 alias gs='git s'
+alias gs.='git s .'
 alias gd='git d'
+alias gd.='git d .'
 alias gds='git ds'
+alias gds.='git ds .'
 alias gc='git c'
 alias gca='git ca'
 alias gap='git ap'
