fa3c2cdf7b9777558c21d6c420e89dce2416f65b
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2026-04-18 09:09:41 -0700

    add more docker aliases

--- .zsh.d/50-aliases-docker.zsh
+++ .zsh.d/50-aliases-docker.zsh
@@ -1,3 +1,9 @@
 alias d='docker'
+alias dps='docker ps'
 alias dc='docker compose'
+alias dcps='docker compose ps'
 alias compose='docker compose'
+
+dcrestart() {
+  docker compose restart "$1" && docker compose logs --follow --tail 5 "$1"
+}
