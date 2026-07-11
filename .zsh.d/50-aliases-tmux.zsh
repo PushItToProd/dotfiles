24a93e402a3e159be9990eef44c3b8266e7363cf
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2026-07-11 06:24:30 -0700

    add helpful tmux aliases

--- .zsh.d/50-aliases-tmux.zsh
+++ .zsh.d/50-aliases-tmux.zsh
@@ -9,3 +9,25 @@ __tmux() {
 }
 
 alias tmux=__tmux
+
+alias tls='tmux ls'
+alias tnew='tmux new'
+alias ta0='tmux a -t 0'
+alias ta1='tmux a -t 1'
+alias ta2='tmux a -t 2'
+alias ta3='tmux a -t 3'
+alias ta4='tmux a -t 4'
+alias ta5='tmux a -t 5'
+alias ta6='tmux a -t 6'
+alias ta7='tmux a -t 7'
+alias ta8='tmux a -t 8'
+alias ta9='tmux a -t 9'
+alias t='tmux'
+
+function ta {
+  local arg="$1"
+  case "$arg" in
+    ''|-*) tmux a "$@" ;;
+    *) tmux a -t "$@" ;;
+  esac
+}
