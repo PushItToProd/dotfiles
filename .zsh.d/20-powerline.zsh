647eef7d04d0a0fba25ec7fde2d09de8c10a2445
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2026-07-12 08:06:34 -0700

    check if powerline-shell is installed and warn if not

--- .zsh.d/20-powerline.zsh
+++ .zsh.d/20-powerline.zsh
@@ -1,22 +1,33 @@
 # -*- mode: sh -*-
 [[ "$DEBUG" == "1" ]] && echo "Configuring powerline"
 
+if ! command -v powerline-shell &>/dev/null; then
+  if [[ ! "$ZSH_D_DISABLE_POWERLINE_WARNING" ]]; then
+    echo "warning: powerline-shell is not installed" >&2
+  fi
+  return
+fi
+
 function powerline_precmd() {
-    # Hack required for ~/bin/powerline_segments/wrap.py to properly wrap text.
-    read TERM_ROWS TERM_COLUMNS < <(stty size)
-    export TERM_ROWS TERM_COLUMNS
-    PS1="$(powerline-shell --shell zsh $?)"
+  # Hack required for ~/bin/powerline_segments/wrap.py to properly wrap text.
+  read TERM_ROWS TERM_COLUMNS < <(stty size)
+  export TERM_ROWS TERM_COLUMNS
+  PS1="$(powerline-shell --shell zsh $?)"
 }
 
 function install_powerline_precmd() {
-    for s in "${precmd_functions[@]}"; do
-        if [ "$s" = "powerline_precmd" ]; then
-            return
-        fi
-    done
-    precmd_functions+=(powerline_precmd)
+  for s in "${precmd_functions[@]}"; do
+    if [ "$s" = "powerline_precmd" ]; then
+      return
+    fi
+  done
+  precmd_functions+=(powerline_precmd)
 }
 
-if [ "$TERM" != "linux" ]; then
-    install_powerline_precmd
+if [[ "$TERM" == "linux" ]]; then
+  if [[ ! "$ZSH_D_DISABLE_POWERLINE_WARNING" ]]; then
+    echo "warning: TERM=$TERM - not powerline-shell precmd hook even though powerline-shell is installed" >&2
+  fi
+  return
 fi
+install_powerline_precmd
