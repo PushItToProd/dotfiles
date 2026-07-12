3a934afbf7391f6725880e11277700e5723d51e9
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2026-07-12 07:59:41 -0700

    only set hledger aliases if it's installed

--- .zsh.d/50-aliases-hledger.zsh
+++ .zsh.d/50-aliases-hledger.zsh
@@ -1,33 +1,37 @@
-# Prefix to hledger commands to use the 2020 journal
-alias l2020="LEDGER_FILE=$HOME/Documents/ledger/2020/2020.journal"
-alias hledger2020="LEDGER_FILE=$HOME/Documents/ledger/2020/2020.journal hledger"
-alias h2020="LEDGER_FILE=$HOME/Documents/ledger/2020/2020.journal hledger"
+if command -v hledger &>/dev/null; then
+  # Prefix to hledger commands to use the 2020 journal
+  if [[ -f "$HOME/Documents/ledger/2020/2020.journal" ]]; then
+    alias l2020="LEDGER_FILE=$HOME/Documents/ledger/2020/2020.journal"
+    alias hledger2020="LEDGER_FILE=$HOME/Documents/ledger/2020/2020.journal hledger"
+    alias h2020="LEDGER_FILE=$HOME/Documents/ledger/2020/2020.journal hledger"
+  fi
 
-alias h='hledger'
+  alias h='hledger'
 
-alias hb='hledger-totals'
+  alias hb='hledger-totals'
 
-alias hbi='hledger-bills'
-alias hbill='hledger-bills'
+  alias hbi='hledger-bills'
+  alias hbill='hledger-bills'
 
-alias he='hledger-envelopes'
-alias hen='hledger-envelopes'
-alias hex='hledger-expenses'
+  alias he='hledger-envelopes'
+  alias hen='hledger-envelopes'
+  alias hex='hledger-expenses'
 
-alias hg='hledger-git'
-alias hgs='hledger-git s'
-alias hga='hledger-git add'
-alias hgc='hledger-git c'
-alias hgca='hledger-git ca'
-alias hgd='hledger-git d'
-alias hgds='hledger-git ds'
-alias hgp='hledger-git push'
+  alias hg='hledger-git'
+  alias hgs='hledger-git s'
+  alias hga='hledger-git add'
+  alias hgc='hledger-git c'
+  alias hgca='hledger-git ca'
+  alias hgd='hledger-git d'
+  alias hgds='hledger-git ds'
+  alias hgp='hledger-git push'
 
-alias hl='hledger-launch'
-alias hle='hledger-launch emacs'
+  alias hl='hledger-launch'
+  alias hle='hledger-launch emacs'
 
-alias hsh='hledger-shell'
+  alias hsh='hledger-shell'
 
-alias ht='hledger-true'
+  alias ht='hledger-true'
 
-alias hv='hledger-validate'
+  alias hv='hledger-validate'
+fi
