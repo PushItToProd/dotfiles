c5a3547433c1ec405159e057b009f010c16d5f93
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2026-07-12 07:34:06 -0700

    set terminal title in tmux/screen

--- .zsh.d/20-terminal-title.zsh
+++ .zsh.d/20-terminal-title.zsh
@@ -5,13 +5,16 @@ set_term_title() {
       ;;
     *xterm*|rxvt|(dt|k|E)term) print -n "\e]2;$__term_title\a"
       ;;
+    screen) printf '\033]2;%s\033\\' "$__term_title"
+      ;;
   esac
 }
 
 # via 3.6 here: https://zsh.sourceforge.io/FAQ/zshfaq03.html
 set_term_title_to_pwd() {
   [[ -t 1 ]] || return
-  set_term_title "$(print -P %~)"
+  _last_set_term_title="$(print -P %~)"
+  set_term_title "$_last_set_term_title"
 }
 
 # Set the terminal title right away.
